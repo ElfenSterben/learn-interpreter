fb9699364ee45dfa66c8a07d1a9f361ad6559825
es <362051950@qq.com>
2024-06-25 18:05:47 +0800

    parser

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,36 +6,6 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
-	l := New(input)
-
-	for i, tt := range tests {
-		tk := l.NextToken()
-		if tk.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tk.Type)
-		}
-		if tk.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tk.Literal)
-		}
-	}
-}
-
-func TestNextToken3(t *testing.T) {
 	input := `let five = 5; 
     let ten = 10; 
     let add = fn(x, y) { 
